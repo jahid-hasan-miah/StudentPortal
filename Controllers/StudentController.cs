71fbeeef5e2b5d060b1fad001633f43fcbb02aef
Md Jahid Hasan <joyh304@gmail.com>
2021-04-17 16:06:45 +0600

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Portal.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult GetAllStudent()
+        {
+            return View();
+        }
+
+        public IActionResult StudentById()
+        {
+            return View();
+        }
+
+        public IActionResult AddNewStudent()
+        {
+            return View();
+        }
+    }
+}
